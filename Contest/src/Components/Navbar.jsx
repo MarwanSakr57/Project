70dc37335ebf63c2f3300d4fe7426aa7bae09170
MarwanSakr57 <Marwansakr5704@gmail.com>
2026-05-02 18:24:45 +0300

    add basic navbar

--- Contest/src/Components/Navbar.jsx
+++ Contest/src/Components/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
-export default function Navbar() {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom'
+export default function NavBar() {
   return (
-    <div>
-      
+<div>
+    <Navbar bg="light"  className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Campus Event Hub</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/Events">Events</Nav.Link>
+            <Nav.Link as={Link} to="/CreateEvents">Create Event</Nav.Link>
+            <Nav.Link as={Link} to="/MyEvents">My Events</Nav.Link>
+          </Nav>
+      </Container>
+    </Navbar>
     </div>
   )
 }
